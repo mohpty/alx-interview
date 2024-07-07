dcf8ffe6636fc13fdb186189591f9332f2c0a41c
Mohammad Fathi <mohfabdou@gmail.com>
2024-07-08 02:25:14 +0300

    fixing issues

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,27 +1,16 @@
 #!/usr/bin/python3
 '''Alx-interviews Lockboxes'''
 
-
-def dfs(current, graph, visited, total):
-    for edge in graph[current]:
-        if not visited[edge]:
-            visited[edge] = 1
-            total += 1
-            if total == len(graph.keys()):
-                return True
-            return dfs(edge, graph, visited, total)
-    return False
-
-
 def canUnlockAll(boxes):
-    '''Solving the lockboxes challenge'''
-
-    # Store the graph
-    graph = {}
-    for i in range(len(boxes)):
-        graph[i] = boxes[i]
-    # create the visited array
-    visited = [0 for i in range(len(boxes))]
-
-    # traverse, mark, repeat
-    return dfs(0, graph, visited, 1)
+    '''check if all boxes can be opened'''
+    if (type(boxes) is not list or len(boxes) == 0):
+        return False
+    for k in range(1, len(boxes) - 1):
+        unlocked = False
+        for i in range(len(boxes)):
+            unlocked = k in boxes[i] and k != i
+            if unlocked:
+                break
+        if unlocked is False:
+            return unlocked
+    return True
